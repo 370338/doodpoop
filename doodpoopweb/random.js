c2318e0c5ce71d5552e7d1804aac7da8cefecd3f
370338 <93786383+370338@users.noreply.github.com>
2022-07-20 13:45:17 -0700

    Delete doodpoopweb directory

--- doodpoopweb/random.js
+++
@@ -1,8 +0,0 @@
-function test() {
-          
-  const possibleVideos = ["index2.html", "index3.html", "index4.html", "index5.html", "index6.html", "index7.html", "index8.html","index9.html", "index10.html", "index11.html"];
-  const randomVideo = possibleVideos[Math.floor(Math.random() * possibleVideos.length)];
-
-  window.location.href = randomVideo;
-}
-
